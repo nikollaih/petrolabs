9df5d46ffe5e2b052be20ea22a570cf27848fda4
Nikollai Hernandez <nikollaihernandez@gmail.com>
2018-02-15 16:47:19 -0500

    Vista para el login

---
+++ application/views/includes/tags.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Login | Petrolabs Admin</title>
+		<!-- Bootstrap -->
+		<link href="<?= base_url() ?>resources/css/bootstrap.min.css" rel="stylesheet">
+		<!-- slimscroll -->
+		<link href="<?= base_url() ?>resources/css/jquery.slimscroll.css" rel="stylesheet">
+		<!-- Fontes -->
+		<link href="<?= base_url() ?>resources/css/font-awesome.min.css" rel="stylesheet">
+		<link href="<?= base_url() ?>resources/css/simple-line-icons.css" rel="stylesheet">
+		<!-- all buttons css -->
+		<link href="<?= base_url() ?>resources/css/buttons.css" rel="stylesheet">
+		<!-- animate css -->
+		<link href="<?= base_url() ?>resources/css/animate.css" rel="stylesheet">
+		<!-- adminbag main css -->
+		<link href="<?= base_url() ?>resources/css/main.css" rel="stylesheet">
+		<!-- darkblue theme css -->
+		<link href="<?= base_url() ?>resources/css/darkblue.css" rel="stylesheet">
+		<!-- media css for responsive  -->
+		<link href="<?= base_url() ?>resources/css/main.media.css" rel="stylesheet">
+		<!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+		<!--[if lt IE 9]> <script src="dist/html5shiv.js"></script> <![endif]-->
+	</head>
