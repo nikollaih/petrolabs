807e7779f66455a5fa0453bcd0e9405f12396a19
Nikollai Hernandez <nikollaihernandez@gmail.com>
2018-02-21 22:13:28 -0500

    Modelos primera parte

--- application/models/usuarios.php
+++ application/models/usuarios.php
@@ -1,8 +1,7 @@
-<?
+<?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * author: Nikollai Hernandez
- * date: 05/07/2016
  */
 
 class Usuarios extends CI_Model{
@@ -19,6 +18,7 @@ class Usuarios extends CI_Model{
 		$this->db->join('ciudades c', 'c.id_ciudad = u.ciudad');
 		$this->db->join('Isleros i', 'i.usuario = u.id_usuario', 'left');
 		$this->db->join('estaciones e', 'e.id_estacion = i.estacion', 'left');
+		$this->db->join('tipos_incentivos ti', 'ti.id_tipo = i.incentivo', 'left');
 		$this->db->where('u.email', $email);
 		$this->db->where('u.clave', md5($password));
 		$this->db->where('u.estado', 1);
---
+++ application/models/ventas.php
@@ -0,0 +1,167 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * author: Nikollai Hernandez
+ */
+
+class Ventas extends CI_Model{
+
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
+
+	function agregarVenta($venta){
+		$this->db->insert('ventas', $venta);
+		return $this->db->last_id();
+	}
+
+	function obtenerVentas(){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasIslero($id_islero){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('v.islero', $id_islero);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasFecha($fecha_inicial, $fecha_final){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('fecha >=', $fecha_inicial);
+		$this->db->where('fecha <=', $fecha_final);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasIsleroFecha($fecha_inicial, $fecha_final, $id_islero){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('v.islero', $id_islero);
+		$this->db->where('fecha >=', $fecha_inicial);
+		$this->db->where('fecha <=', $fecha_final);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasFechaPago($fecha_inicial, $fecha_final){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('fecha_pago >=', $fecha_inicial);
+		$this->db->where('fecha_pago <=', $fecha_final);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasIsleroFechaPago($fecha_inicial, $fecha_final, $id_islero){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('v.islero', $id_islero);
+		$this->db->where('fecha_pago >=', $fecha_inicial);
+		$this->db->where('fecha_pago <=', $fecha_final);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasProducto($id_producto){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('v.producto', $id_producto);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasProductoFecha($fecha_inicial, $fecha_final, $id_producto){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('fecha >=', $fecha_inicial);
+		$this->db->where('fecha <=', $fecha_final);
+		$this->db->where('v.producto', $id_producto);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function obtenerVentasProductoFechaPago($fecha_inicial, $fecha_final, $id_producto){
+		$this->db->from('ventas v');
+		$this->db->join('isleros i', 'v.islero = i.id_islero');
+		$this->db->join('productos p', 'v.producto = p.id_producto');
+		$this->db->where('fecha_pago >=', $fecha_inicial);
+		$this->db->where('fecha_pago <=', $fecha_final);
+		$this->db->where('v.producto', $id_producto);
+		$objVentas = $this->db->get();
+
+		if ($objVentas->num_rows() > 0) {
+			return $objVentas->result_array();
+		}
+		else{
+			return 0;
+		}
+	}
+
+	function eliminarVenta($id_venta){
+		$this->db->where('id_venta', $id_venta);
+		return $this->db->delete('ventas');
+	}
+}
